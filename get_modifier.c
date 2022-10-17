ab91d8387a49cbad5847df833dcf134b1ef8b001
abdee445 <abdallahmusa300@yahoo.com>
2022-10-17 11:06:45 -0700

    get_modifier.c2

--- get_modifier.c
+++ get_modifier.c
@@ -31,7 +31,7 @@ modifier_t *get_modifier(const char *s, unsigned int *pos)
 	modif = malloc(sizeof(modifier_t));
 	if (modif == NULL)
 		return (NULL);
-	modif->flags = get_flags(s, &i);
+	modif ->flags = get_flags(s, &i);
 	modif->width = get_width(s, &i);
 	modif->precision = get_precision(s, &i);
 	modif->length = get_length(s, &i);
