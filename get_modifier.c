490062a13786c286e9eff78d4699b72bb3238eb0
abdee445 <abdallahmusa300@yahoo.com>
2022-10-17 11:08:17 -0700

    get_modifier.c5

--- get_modifier.c
+++ get_modifier.c
@@ -34,7 +34,7 @@ modifier_t *get_modifier(const char *s, unsigned int *pos)
 	modif ->flags = get_flags(s, &i);
 	modif ->width = get_width(s, &i);
 	modif ->precision = get_precision(s, &i);
-	modif->length = get_length(s, &i);
+	modif ->length = get_length(s, &i);
 	modif->specifier = get_specifier(s, &i);
 
 	if (!modif->specifier)
